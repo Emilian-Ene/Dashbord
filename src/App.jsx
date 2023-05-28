5c92664134be1dc817f9d8cdce6b4a9554726e85
Ionut Emilian Ene <ionutemilianene@gmail.com>
2023-05-28 15:09:09 +0100

    remove

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="app">
-      <h1>Deploy</h1>
       <Header />
       <Navbar />
       {/* <Reports /> */}
