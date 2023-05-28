aa93b6c1d1d3d8c329900e9ed0314f731f2974f0
Ionut Emilian Ene <ionutemilianene@gmail.com>
2023-05-28 15:15:45 +0100

    remove p

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="app">
-      <p>jfgsdhfgsjfgshff</p>
       <Header />
       <Navbar />
       {/* <Reports /> */}
